5e24fc5d9aec70a754e15964fa25069a5c586ad6
INDEZENDE <i.rios.zendejas@gmail.com>
2018-03-06 01:50:11 -0600

    Genero el segundo request, la información se envia a consola

--- js/app.js
+++ js/app.js
@@ -6,13 +6,13 @@ fetch("https://swapi.co/api/films/")
         return response.json();
     })
     .then(function (data) {
+
       // Contiene los títulos
       let containerGallery= document.getElementById('site-container');
       let output = ' ';
 
       data.results.forEach(function(element) {
         output +=
-
         `<h4 class = "${"card-title"}">${"Name"}</h4>
           <p class = "${"card-text"}">${element.title}</p>
           <br>
@@ -20,7 +20,6 @@ fetch("https://swapi.co/api/films/")
           <p class = "${"card-text"}">${element.episode_id}</p>
           <br>
           <h4 class = "${"card-title"}">${"Characters"}</h4>
-
           <br>`
           let output2 = ' ';
           element.characters.forEach(function(element1){
@@ -28,13 +27,27 @@ fetch("https://swapi.co/api/films/")
             `<p class = "${"card-text"}">${element1}</p>
             <br>`
             containerGallery.innerHTML = output2;
-            console.log(typeof(element.characters.element1));
+            // console.log(element1);
+            $.ajax({//Segunda petición
+              url: element1
+          }).done(gettingCharacters);
+
+          // Inicia la función para obtener el segundo request
+
+          function gettingCharacters (element){//Función que depende de la segunda petición
+            console.log(element.name);
+            console.log(element.height);
+            console.log(element.mass);
+            console.log(element.hair_color);
+            console.log(element.skin_color);
+          }
+
+
+
+
           })
         containerGallery.innerHTML = output;
-        //
-
         });
-
 
     })
     .catch(function (error) {
