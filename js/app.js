35e9ce1c48faa35e34d8cd28351df6e01314e9f2
INDEZENDE <i.rios.zendejas@gmail.com>
2018-03-07 20:47:40 -0600

    Mejorando la sintaxis del código[C

--- js/app.js
+++ js/app.js
@@ -1,54 +1,63 @@
-var sectionPeople = document.getElementById('site-container');
-
-fetch("https://swapi.co/api/films/")
-    .then(response = response => {
+var url= "https://swapi.co/api/films/"
+fetch(`${url}`)
+    .then(function (response) {
         return response.json();
     })
-    .then(titles = data => {
-      // Contiene los títulos
-      let containerGallery= document.getElementById('site-container');
-      let output = ' ';
-      let count =1;
-      data.results.forEach(moviesDetails = element =>{
-        output +=
-        ` <img src="assets/images/e${count++}${".jpg"}">
-          <br>
-          <h4 class = "${"card-title"}">${"Name"}</h4>
-          <p class = "${"card-text"}">${element.title}</p>
-          <br>
-          <h4 class = "${"card-title"}">${"Episodes"}</h4>
-          <p class = "${"card-text"}">${element.episode_id}</p>
-          <br>
-          <h4 class = "${"card-title"}">${"Characters"}</h4>
-          <br>`
-          element.characters.forEach(function(element1){
-            output +=//Guarda las características de cada personaje
-            `<p class = "${"card-text"}" data-toggle="${"modal"}" data-target="${"#modalCharacters"}" data-whatever="${"@mdo"}">${element1} </p>
-            <br>`
-            containerGallery.innerHTML = output;
-            // console.log(element1);
-
-            $.ajax({//Segunda petición
-              url: element1
-          }).done(gettingCharacters);
-
-          // Inicia la función para obtener el segundo request
-
-          function gettingCharacters (element){//Función que depende de la segunda petición
-
-            console.log(element.name);
-            console.log(element.height);
-            console.log(element.mass);
-            console.log(element.hair_color);
-            console.log(element.skin_color);
-          }
-
-          })
-        containerGallery.innerHTML = output;
-        });
-
-
+    .then(function (data) { //Contiene el json de la consulta
+        // console.log('Request successful');
+        let results = data.results;
+        // console.log(results);//Contiene el arreglo de objetos con las 7 películas
+        printFilms(results); //Función que pinta los resultados de las películas
     })
     .catch(function (error) {
         console.log('Request failed', error)
     });
+
+// Función que pinta películas
+
+let printFilms =results => {
+  let containerGallery= document.getElementById('site-container');
+  let output = ' ';
+  let count =1;
+  // Pinta las películas
+  results.forEach(moviesDetails = element =>{
+    output += //Guarda las características de cada película
+    ` <img class = "${"films-img"}" src="assets/images/e${count++}${".jpg"}">
+      <br>
+      <h4 class = "${"card-title"}">${"Name"}</h4>
+      <p class = "${"card-text"}">${element.title}</p>
+      <br>
+      <h4 class = "${"card-title"}">${"Episodes"}</h4>
+      <p class = "${"card-text"}">${element.episode_id}</p>
+      <br>
+      <h4 class = "${"card-title"}">${"Characters"}</h4>
+      <br>`
+      //Pina los personajes
+      element.characters.forEach(function(element1){
+        output +=//Guarda las características de cada personaje
+        `<p class = "${"card-text"}" data-toggle="${"modal"}" data-target="${"#modalCharacters"}" data-whatever="${"@mdo"}">${element1} </p>
+        <br>`
+        gettingCharacters (element1)
+        });
+        });
+        containerGallery.innerHTML = output;
+}
+//Inicia la seggunda petición
+let gettingCharacters = characters => {
+
+  fetch(`${characters}`)
+      .then(function (response) {
+          return response.json();
+      })
+      .then(function (data) { //Contiene el json de la consulta
+          // console.log('Request successful');
+          let results = data;
+          // console.log(results);//Contiene el arreglo de objetos con las 7 películas
+          printFilms(results); //Función que pinta los resultados de las películas
+      })
+      .catch(function (error) {
+          console.log('Request failed', error)
+      });
+
+
+}
